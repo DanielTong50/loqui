17abce9021acd873ae6a93ee67034cb3138195df
reidpat <61036489+reidpat@users.noreply.github.com>
2022-08-01 16:49:03 -0700

    first demo commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
+<script>
+    import Example from "../components/example.svelte";
+
+
+    let x = 0;
+
+    function handleClick(){
+        x++;
+    }
+
+    //https://svelte.dev/tutorial/svelte-window
+    let key;
+    function handleKeydown(event){
+        key = event.key;
+        if(key == 1){
+            //play phrase 1
+        }
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Example />
+
+<button on:click={handleClick}>Clicked on {x} times</button>
+<svelte:window on:keydown={handleKeydown}/>
+<p>{key}</p>
+
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
