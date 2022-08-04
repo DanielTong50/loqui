d0e366e60d410a475b582dcd3973e2558177139c
rachel-deng <rachel05deng@gmail.com>
2022-08-04 00:21:25 -0700

    updated image alt text in logo

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,5 +1,16 @@
 <div class="title">
-    <img src="" alt="Logo Image" />
+    <img src = "" alt="Logo" />
     <h1>LOQUI</h1>
     <p>Assistive Technology</p>
 </div>
+
+<style>
+    .title {
+        align-items: center;
+    }
+
+    img {
+        width: 150px;
+        margin: 0;
+    }
+</style>
