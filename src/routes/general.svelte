5f51e06703616a7eb0f3a4a41ebd09eab702198c
Hash-Daniel <97985602+DanielTong50@users.noreply.github.com>
2022-08-07 09:41:44 -0700

    Update general.svelte

--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -10,7 +10,30 @@
         if(key == 0){
             goto('home');
         }
+
+        
 	}
 
 </script>
 <svelte:window on:keydown={handleKeydown} />
+
+<div class="phrases">
+    <PhraseOptions num=1 description="Thanks."/>
+    <PhraseOptions num=2 description="Sorry."/>
+    <PhraseOptions num=3 description="Excuse me."/>
+    <PhraseOptions num=4 description="Hello!"/>
+    <PhraseOptions num=5 description="Could you help me with this please?"/>
+    <PhraseOptions num=6 description="I'm hungry."/>
+</div>
+
+<div class = "instructions">
+    <p>Press the number corresponding to the phrase. Press 0 to always return to the home page.</p>
+</div>
+
+<style>
+    .instructions {
+        color: gray;
+        font-family: "Inter";
+        font-style: italic;
+    }
+</style>
