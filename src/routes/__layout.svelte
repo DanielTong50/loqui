be38d6dfa33b9ba95ced9e068eeb89db12e09512
rachelkd <rachel05deng@gmail.com>
2023-06-27 18:19:29 -0700

    Font changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,13 +9,15 @@
 </div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
 	.main-content {
+		font-family: "Poppins", sans-serif;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: space-between;
 		height: calc(0vh - 100px);
-		font-family: 'Gill Sans', sans-serif;
         padding: 50px;
 	}
 </style>
